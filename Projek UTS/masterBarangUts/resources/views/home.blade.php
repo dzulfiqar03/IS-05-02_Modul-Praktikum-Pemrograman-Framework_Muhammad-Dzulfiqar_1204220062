75e9c3e3758d5d68e2470aa46b7007381dd6f435
Dzulfiqar03 <muhammaddzulfiqar03@gmail.com>
2024-05-02 09:58:44 +0700

    Mini Project Framework - Muhammad Dzulfiqar

--- Projek UTS/masterBarangUts/resources/views/home.blade.php
+++ Projek UTS/masterBarangUts/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initialscale=1.0">
-    <title>Belajar Controller & View pada Laravel</title>
+    <title>Home</title>
 
     @vite('resources/sass/app.scss')
 
